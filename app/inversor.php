d4849b29260864e5ec4f903ed0599ae62d4af6b5
rolando <rolandodel25@gmail.com>
2019-12-26 10:30:55 -0300

    commit inicial

---
+++ app/inversor.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class inversor extends Authenticatable
+{
+    use Notifiable;
+    public $table ="inversores";
+}
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Registrar') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="invApyNombre" class="col-md-4 col-form-label text-md-right">{{ __('Apellido y Nombre') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="invApyNombre" type="text" class="form-control @error('invApyNombre') is-invalid @enderror" name="invApyNombre" value="{{ old('invApyNombre') }}" required autocomplete="invApyNombre" autofocus>
+
+                                @error('invApyNombre')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="invCorreo" class="col-md-4 col-form-label text-md-right">{{ __('Correo') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="invCorreo" type="email" class="form-control @error('invCorreo') is-invalid @enderror" name="invCorreo" value="{{ old('invCorreo') }}" required autocomplete="invCorreo">
+
+                                @error('invCorreo')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="invCelular" class="col-md-4 col-form-label text-md-right">{{ __('Celular') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="invCelular" class="form-control @error('invCelular') is-invalid @enderror" name="invCelular" value="{{ old('invCelular') }}" required autocomplete="invCelular">
+
+                                @error('invCelular')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="invPassword" class="col-md-4 col-form-label text-md-right">{{ __('Clave') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="invPassword" type="invPassword" class="form-control @error('invPassword') is-invalid @enderror" name="invPassword" required autocomplete="new-invPassword">
+
+                                @error('invPassword')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Registrar') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
